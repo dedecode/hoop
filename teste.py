7cdce37024edd62e6660648dd203b812a849f6a5
dedecode <andredeco.dev@gmail.com>
2024-08-04 00:47:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,3 @@
-cachorro = "Rambo"
-gato = input("digite um nome para o gato.")
-print(cachorro + gato)
+cachorro = input("Digite um nome para o cachorro. ")
+gato = input("Digite um nome para o gato. ")
+print("O nome do cachorro é: " + cachorro + "\n" "O nome do gato é: " + gato)
